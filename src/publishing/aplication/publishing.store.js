cbda1abf94801c51de6622ed32959276da0d3570
Fabrizio <robertogonzalesayala@gmail.com>
2026-04-27 18:32:54 -0500

    feat(categories): add categories

---
+++ src/publishing/aplication/publishing.store.js
@@ -0,0 +1,77 @@
+import {PublishingApi} from "../infrastructure/publishing-api.js";
+import {defineStore} from "pinia";
+import {computed, ref} from "vue";
+import {CategoryAssembler} from "../infrastructure/category.assembler.js";
+
+const publishingApi = new PublishingApi();
+
+const usePublishingStore = defineStore('publishing', () => {
+    const categories = ref([]);
+    const errors = ref([]);
+    const categoriesLoaded = ref([false]);
+    const categoriesCount = computed(() => {
+        return categoriesLoaded ? categories.value.lenght : 0 ;
+    });
+
+    function fetchCategories(){
+        publishingApi.getCategories().then((response) => {
+            categories.value = CategoryAssembler.toEntitiesFromResponse(response);
+            categoriesLoaded.value = true;
+            console.log(categoriesLoaded.value);
+            console.log(categories.value);
+        }).catch((error) => {
+            errors.value.push(error);
+        });
+    }
+
+    function getCategoryById(id){
+        let idNum = parseInt(id);
+        return categories.value.find(category => category['id'] === idNum);
+    }
+
+    function addCategory(category){
+        publishingApi.createCategory(category).then((response) => {
+            const resource =response.data;
+            const newCategory = CategoryAssembler.toEntityFromResource(resource);
+            categories.value.push(newCategory);
+        }).catch((error) => {
+            errors.value.push(error);
+        });
+    }
+
+    function updateCategory(category){
+        publishingApi.updateCategory(category).then((response) => {
+            const resource = response.data;
+            const updateCategory = CategoryAssembler.toEntityFromResource(resource);
+            const index = category.value.findIndex(c => c['id'] === resource.id);
+            if(index !== -1) categories.value[index]= updateCategory;
+        }).catch((error) => {
+            errors.value.push(error);
+        });
+    }
+
+    function deleteCategory(category){
+        publishingApi.deleteCategory(category).then((response) => {
+            const index = categories.value.findIndex(c => c['id'] === category.id);
+            if(index !== -1) categories.value.splice(index, 1);
+        }).catch((error) => {
+            errors.value.push(error);
+        });
+    }
+
+    return{
+        categories,
+        errors,
+        categoriesLoaded,
+        categoriesCount,
+        fetchCategories,
+        getCategoryById,
+
+
+        addCategory,
+        updateCategory,
+        deleteCategory,
+    }
+});
+
+export default usePublishingStore;
